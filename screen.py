3672e8df6233dd21eb7b89218488ef1fa8aea995
Ren22 <ngm.renat@gmail.com>
2017-06-22 11:21:44 +0200

    2 param variation

--- screen.py
+++ screen.py
@@ -1,11 +1,13 @@
 import os
 import numpy as np
 
-au = 0.2 # activator kinetic constant
-Du = 1 # activator diffusion constant
+#au = 0.2 # activator kinetic constant
+#Du = 1 # activator diffusion constant
 time_period = 50
-times = 1
+times = 30
 
-for size in np.arange(40, 121, 5):
-    size_segments = 2 * size
-    os.system("python wavesearch.py {0} {1} {2} {3} {4} {5}".format(size, size_segments, time_period, times, au, Du))
+for au in np.arange(0.17, 0.26, 0.01):
+    for Du in np.arange(0.7, 1.3, 0.1):
+        for size in np.arange(40, 121, 5):
+            size_segments = 2 * size
+            os.system("python wavesearch.py {0} {1} {2} {3} {4} {5}".format(size, size_segments, time_period, times, au, Du))
