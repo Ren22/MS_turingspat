e32a4a32d6f68c2ec4f6a8bfcfdef5712b1c6098
Ren22 <ngm.renat@gmail.com>
2017-06-22 11:27:58 +0200

    backuped

--- screen.py
+++ screen.py
@@ -4,7 +4,7 @@ import numpy as np
 au = 0.2 # activator kinetic constant
 Du = 1 # activator diffusion constant
 time_period = 50
-times = 1
+times = 30
 
 for size in np.arange(40, 121, 5):
     size_segments = 2 * size
