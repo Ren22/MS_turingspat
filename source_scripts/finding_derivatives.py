dd3dd24dec8f411129d03a37af78cca64630306b
Ren22 <ngm.renat@gmail.com>
2017-08-28 13:28:59 +0200

    Model 4 run

--- source_scripts/finding_derivatives.py
+++ source_scripts/finding_derivatives.py
@@ -1,10 +1,11 @@
 from sympy import *
 import sympy
 import numpy as np
+import pandas as pd
 
-a, b, c, d, au, u0, v0, n, M, u, v, m = symbols('a, b, c, d, au, u0, v0, n, M, u, v, m ')
+a, b, c, d, au, u0, v0, n, M, u, v = symbols('a, b, c, d, au, u0, v0, n, M, u, v ')
 
-f = a * (u - u0) * M ** n + b * (v - v0) * M ** n - (au * (u - u0) ** 3)
+f = a * (u - u0) * M ** n + b * (v - v0) - (au * (u - u0) ** 3)
 g = c * (u - u0) * M ** n + d * (v - v0) * M ** n
 
 fu = diff(f, u)
@@ -13,20 +14,27 @@ gu = diff(g, u)
 gv = diff(g, v)
 
 # print('fu = {0}, fv = {1}, gu ={2}, gv = {3}'.format(fu, fv, gu, gv))
-
+Du = 1.
+Dv = 20.
 numerator = (sympy.sqrt(fu * gv - fv * gu) + sympy.sqrt(-fv * gu)) ** 2
 denominator = (fu ** 2)
 
 print ('numerator/denominator = {0}'.format(numerator / denominator))
-M = 100. * (1. / 10)
+# M = 100. * (1. / 10)
 result = numerator / denominator
-result_subs = result.subs([(a, 0.2), (b, -0.4), (c, 0.6), (d, -0.8), (au, 5.), (u0, 0.5), (v0, 0.5), (n, -2.)])
-print(result_subs)
-#
-# for size in np.arange(10., 120., 1.):
-#     discretizing_factor = 6.
-#     size_segments = discretizing_factor * size
-#     x = np.linspace(0., size, size_segments)
-#     result_subs = result.subs([(a, 0.2), (b, -0.4), (c, 0.6), (d, -0.8),
-#                                (au, 5.), (u0, 0.5), (v0, 0.5), (n, -2.), (m, 100. * (1. / x[-1]))])
-#     print(result_subs)
+
+for size in np.arange(10., 120., 1.):
+    result_subs = result.subs([(a, 0.2), (b, -0.4), (c, 0.6), (d, -0.8),
+                               (au, 5.), (u0, 0.5), (v0, 0.5), (n, -2.), (M, 100. * (1. / size)),
+                               (u, 0.5), (v, 0.5)])
+
+    df_new = pd.DataFrame([[size, Dv / Du, result_subs]],
+                          columns=['size', 'Dv/Du', 'Model #'])
+    try:
+        df = pd.read_csv('results/instability.csv')
+        df = df.append(df_new, ignore_index=True)
+    except:
+        df = df_new
+
+    df.to_csv('results/instability.csv', index=False)
+    # print(result_subs)
